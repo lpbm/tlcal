b0c348859b5b74a822eec0efa530338ad6b98239
Marius Orcsik <marius.orcsik@rocket-internet.de>
2015-09-25 12:58:27 +0200

    Refacored the url builder class to enable urls for event info

--- liquid/scraper/html.py
+++ liquid/scraper/html.py
@@ -1,23 +1,42 @@
 from datetime import datetime
-from requests import get, codes
+from requests import get, post, codes
+import json
 
 
 class Html:
     """
     """
 
-    uris = {
-        "sc2": "http://www.teamliquid.net/calendar/",
-        "hrt": "http://www.liquidhearth.com/calendar/",
-        "dot": "http://www.liquiddota.com/calendar/",
-        "lol": "http://www.liquidlegends.net/calendar/"
+    base_uris = {
+        "sc2": "http://www.teamliquid.net",
+        "hrt": "http://www.liquidhearth.com",
+        "dot": "http://www.liquiddota.com",
+        "lol": "http://www.liquidlegends.net"
+    }
+
+    calendar_path = {
+        "sc2": "/calendar",
+        "hrt": "/calendar",
+        "dot": "/calendar",
+        "lol": "/calendar"
+    }
+
+    event_path = {
+        "sc2": "/calendar/manage",
+        "hrt": "/calendar/manage",
+        "dot": "/calendar/manage",
+        "lol": "/calendar/manage"
     }
 
     class UriBuilder:
         """
         """
         @staticmethod
-        def get_uri(calendar="sc2", by_week=True, by_month=False, date=None):
+        def get_uri(calendar="sc2"):
+            return Html.base_uris[calendar]
+
+        @staticmethod
+        def get_calendar_uri(calendar="sc2", by_week=True, by_month=False, date=None):
             if by_week:
                 view_by = "week"
             if by_month:
@@ -27,7 +46,11 @@ class Html:
                 date = datetime.now()
 
             fmt = date.strftime
-            return Html.uris[calendar] + "?view=%s&year=%s&month=%s&day=%s" % (view_by, fmt("%Y"), fmt("%m"), fmt("%d"))
+            return Html.base_uris[calendar] + Html.calendar_path[calendar] + "?view=%s&year=%s&month=%s&day=%s" % (view_by, fmt("%Y"), fmt("%m"), fmt("%d"))
+
+        @staticmethod
+        def get_event_uri(calendar="sc2"):
+            return Html.base_uris[calendar] + Html.event_path[calendar]
 
     @staticmethod
     def get_calendar(calendar="sc2", by_week=True, by_month=False, date=None, debug=False):
@@ -35,7 +58,7 @@ class Html:
         :return: str
         """
 
-        calendar_uri = Html.UriBuilder.get_uri(calendar, by_week, by_month, date)
+        calendar_uri = Html.UriBuilder.get_calendar_uri(calendar, by_week, by_month, date)
         if debug:
             print("Loading calendar from: %s" % calendar_uri)
 
@@ -44,3 +67,26 @@ class Html:
             return tl_response.content
         else:
             return ""
+
+    @staticmethod
+    def get_event(calendar="sc2", event_id=None, debug=True):
+        if event_id is None:
+            return ""
+
+        if debug:
+            print(event_id, end=" ", flush=True)
+
+        html = ""
+        event_uri = Html.UriBuilder.get_event_uri(calendar)
+
+        post_data = {
+            "action": "view-event-popup",
+            "event_id": event_id
+        }
+
+        tl_response = post(event_uri, post_data)
+        if tl_response.status_code == codes.ok and tl_response.headers.get('content-type') == "application/json":
+            decoded_response = json.loads(tl_response.content.decode(encoding="UTF-8"))
+            html = decoded_response["html"]
+
+        return html
